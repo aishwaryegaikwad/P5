5a27f2bc45a0a8c540824dc48fe2e227131dca49
aishwaryegaikwad <70074064+aishwaryegaikwad@users.noreply.github.com>
2020-10-15 00:13:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,108 @@
 // Engineered By Aishwarye Gaikwad and Roni Kaufman (https://ronikaufman.github.io)
 
-let kMax,step,n=60,radius=0,inter=.5,maxNoise=1e3,noiseProg=i=>i;var canvas;function windowResized(){resizeCanvas(windowWidth,windowHeight),maxNoise=windowWidth<=320?220:windowWidth<=480?320:windowWidth<=640?500:windowWidth<=720?560:windowWidth<=1080?640:windowWidth<=1440?720:800}function setup(){(canvas=createCanvas(windowWidth,windowHeight)).style("z-index","-100"),canvas.position(0,0),colorMode(HSB,1),angleMode(DEGREES),noFill(),kMax=1,step=.01,strokeWeight(2)}function draw(){background(0);let i=frameCount/100;kMax=noise(i/2),maxNoise=windowWidth<=320?220:windowWidth<=480?320:windowWidth<=640?500:windowWidth<=720?560:windowWidth<=1080?640:windowWidth<=1440?720:800;for(let o=n;o>=0;o--){noiseProg(o/n);o%2==0?fill(mouseX/1800,100,100):fill(0);let e=radius+o*inter,t=kMax*sqrt(o/n),d=maxNoise*noiseProg(o/n);blob(e,width/2,height/2,t,i+o*step,d)}}function blob(i,n,o,e,t,d){beginShape();for(let w=0;w<=374.4;w+=7.2){let s,a;s=cos(w)+1,a=sin(w)+1;let h=i+noise(e*s,e*a,t)*d,r=n+h*cos(w),l=o+h*sin(w);curveVertex(r,l)}endShape()}
+let kMax;
+let step;
+let n = 60; // number of blobs
+let radius = 0; // diameter of the circle
+let inter = 0.5; // difference between the sizes of two blobs
+let maxNoise = 640;
+
+
+let noiseProg = (x) => (x);
+
+var canvas;
+
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
+    if(windowWidth <= 320) {
+        maxNoise = 220;
+    } else if(windowWidth <= 480) {
+        maxNoise = 320;
+    } else if(windowWidth <= 640) {
+        maxNoise = 500;
+    }else if(windowWidth <= 720) {
+        maxNoise = 560;
+    } else if(windowWidth <= 1080){
+        maxNoise = 640;
+    } 
+}
+
+function setup() {
+    frameRate(60);
+  canvas= createCanvas(windowWidth, windowHeight);
+  canvas.style('z-index','-100');
+//   canvas.style('width','100%')
+  canvas.position(0,0);
+
+
+  colorMode(HSB, 1);
+	angleMode(DEGREES);
+    noFill();
+	//noLoop();
+	kMax = 1;
+	step = 0.01;
+	strokeWeight(2);
+}
+
+function draw() {
+
+  background(0);
+	let t = frameCount/100;
+	kMax = noise(t/2);
+  
+    if(windowWidth <= 320) {
+        maxNoise = 220;
+    } else if(windowWidth <= 480) {
+        maxNoise = 320;
+    } else if(windowWidth <= 640) {
+        maxNoise = 500;
+    }else if(windowWidth <= 720) {
+        maxNoise = 560;
+    } else if(windowWidth <= 1080){
+        maxNoise = 640;
+    } 
+
+  for (let i = n; i >= 0; i--) {
+		let alpha = 1 - noiseProg(i / n);
+		if (i % 2 === 0) {
+            fill(mouseX/1800,100,100);
+            // fill(12,mouseX/1000,100);
+		} else {
+			fill(0);
+		}
+		let size = radius + i * inter;
+		let k = kMax * sqrt(i/n);
+		let noisiness = maxNoise * noiseProg(i / n);
+    blob(size, width/2, height/2, k, t + i * step, noisiness);
+  }
+}
+
+function blob(size, xCenter, yCenter, k, t, noisiness) {
+  beginShape();
+	let angleStep = 360 / 50;
+  for (let theta = 0; theta <= 360 + angleStep * 2; theta += angleStep) {
+    let r1, r2;
+		/*
+    if (theta < PI / 2) {
+      r1 = cos(theta);
+      r2 = 1;
+    } else if (theta < PI) {
+      r1 = 0;
+      r2 = sin(theta);
+    } else if (theta < 3 * PI / 2) {
+      r1 = sin(theta);
+      r2 = 0;
+    } else {
+      r1 = 1;
+      r2 = cos(theta);
+    }
+		*/
+		r1 = cos(theta)+1;
+		r2 = sin(theta)+1;
+    let r = size + noise(k * r1,  k * r2, t) * noisiness;
+    let x = xCenter + r * cos(theta);
+    let y = yCenter + r * sin(theta);
+    curveVertex(x, y);
+  }
+  endShape();
+}
